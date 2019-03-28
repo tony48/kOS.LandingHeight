2054217cfe186d6e62e0d81e614b4c88a828fbc2
tony48 <antolinares01@gmail.com>
2019-03-27 20:37:38 -0400

    Upload

---
+++ kOS.LandingHeight/Addon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using kOS.Safe.Encapsulation;
+using kOS.Safe.Encapsulation.Suffixes;
+using UnityEngine;
+
+namespace kOS.AddOns.LandingHeight
+{
+    [kOSAddon("LH")]
+    [Safe.Utilities.KOSNomenclature("LHAddon")]
+    public class Addon : Suffixed.Addon
+    {
+        public Addon(SharedObjects shared) : base (shared)
+        {
+            InitializeSuffixes();
+        }
+
+        private void InitializeSuffixes()
+        {
+            AddSuffix("HEIGHT", new Suffix<ScalarValue>(GetHeight));
+        }
+
+        public ScalarValue GetHeight()
+        {
+            if (Available())
+            {
+                return LHWrapper.LandingHeight();
+            }
+            throw new Exception("Langing Height not found");
+        }
+
+        public override BooleanValue Available()
+        {
+            return LHWrapper.Wrapped();
+        }
+    }
+}
